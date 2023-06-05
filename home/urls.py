bebc2d9defea5b521adf3a6bd0a48142bc9e13da
khalidur-sujon <khalidur.sujon@gmail.com>
2023-06-05 10:06:41 +0600

    Added a url for logout

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('login/', login_view, name='login_view'),
+    path('logout/', logout_view, name='logout_view'),
     path('register/', register_view, name='register_view'),
     path('add-blog/', add_blog, name='add_blog'),
     path('blog-detail/<slug:slug>', blog_detail, name='blog_detail'),
