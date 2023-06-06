df114aeeeaeacd92a95b2ec25fda0cc5ab6b2fed
khalidur-sujon <khalidur.sujon@gmail.com>
2023-06-06 19:34:41 +0600

    Give some padding to update and delete button

--- Blog/settings.py
+++ Blog/settings.py
@@ -134,3 +134,13 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'tes000ing@gmail.com'
+EMAIL_HOST_PASSWORD = 'testing321'
+# DEFAULT_FROM_EMAIL = 'tes000ing@gmail.com'
+# SERVER_EMAIL = 'tes000ing@gmail.com'
