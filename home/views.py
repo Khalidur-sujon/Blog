4edc102ddec0dc95f1c6d4c4d8f6daac2b7124e7
khalidur-sujon <khalidur.sujon@gmail.com>
2023-06-07 11:49:01 +0600

    Added a profile views

--- home/views.py
+++ home/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import logout
 from django.contrib import messages
-from .form import BlogForm
+from django.contrib.auth.decorators import login_required
+from .form import BlogForm, UserUpdateForm, ProfileUpdateForm
 from .models import BlogModel, Profile
+
+
+@login_required
+def user_profile_view(request):
+    user = Profile.objects.filter(user=request.user).first()
+
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect('/profile/')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+        'user': user
+    }
+    return render(request, 'home/profile.html', context)
 
 
 def home(request):
