1c0bcea80170c996f9787dc1a3d841d7d31aa349
khalidur-sujon <khalidur.sujon@gmail.com>
2023-06-07 11:48:05 +0600

    Added userUpdate and Profile UUpdate form

--- home/form.py
+++ home/form.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 from froala_editor.widgets import FroalaEditor
 from .models import *
 
@@ -7,3 +8,17 @@ class BlogForm(forms.ModelForm):
     class Meta:
         model = BlogModel
         fields = ('content',)
+
+
+class UserUpdateForm(forms.ModelForm):
+    username = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ['username']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
