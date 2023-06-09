99f82603df0e5f44fbe4bfa17b1c049dbf2a17f7
khalidur-sujon <khalidur.sujon@gmail.com>
2023-06-09 09:16:51 +0600

    Override profile model's save method to resize bigger image

--- home/models.py
+++ home/models.py
@@ -8,6 +8,8 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from PIL import Image
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -17,6 +19,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def save(self, *args, **kwargs):
+        super.save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 and img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
 
 class BlogModel(models.Model):
